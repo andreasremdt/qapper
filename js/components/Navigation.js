2da49e94e720d31dc44ac53fe1014d06ee892d54
Andreas Remdt <andreas.remdt@gmail.com>
2019-11-28 15:10:59 +0100

    feat(folders): add folders

--- js/components/Navigation.js
+++ js/components/Navigation.js
@@ -5,10 +5,10 @@ function Navigation() {
   return (
     <nav className="border-b border-solid border-gray-300 bg-white">
       <div className="container mx-auto flex">
-        <NavLink href="/" icon="Home">
+        <NavLink href="/" icon="home">
           Dashboard
         </NavLink>
-        <NavLink href="/testcases/create" icon="Folder">
+        <NavLink href="/testcases/create" icon="new-folder">
           Create Testcase
         </NavLink>
       </div>
--- js/pages/Dashboard.js
+++ js/pages/Dashboard.js
@@ -1,7 +1,80 @@
-import { h } from "preact";
+import { h, Component, Fragment } from "preact";
+import { Link } from "preact-router/match";
+import Icon from "../components/Icon";
+import Button from "../components/Button";
 
-function Dashboard() {
-  return <h1>Dashboard</h1>;
+class Dashboard extends Component {
+  render() {
+    return (
+      <Fragment>
+        <details
+          className="border border-solid border-gray-300 bg-white mb-2 rounded-sm"
+          open
+        >
+          <summary className="flex items-center cursor-pointer p-4">
+            <Icon icon="folder" width="18" height="18" className="mr-2" />
+            Cawemo QA
+            <Button
+              icon="plus"
+              simple
+              title="Create a new report"
+              className="ml-auto mr-3"
+            />
+            <Button
+              icon="edit"
+              simple
+              title="Edit this testcase"
+              className="mr-3"
+            />
+            <Button
+              variant="danger"
+              icon="trash"
+              simple
+              title="Delete this testcase"
+            />
+          </summary>
+          <ul className="border-t border-solid border-gray-300">
+            <li className="border-b border-solid border-gray-300">
+              <Link
+                className="flex items-center py-2 pl-8 pr-4"
+                href="/testcases/123/view"
+              >
+                <Icon icon="file" width="16" height="16" className="mr-2" />{" "}
+                Chrome on SaaS
+                <span className="ml-auto text-sm text-gray-500">
+                  90% completed
+                </span>
+              </Link>
+            </li>
+            <li className="border-b border-solid border-gray-300">
+              <Link
+                className="flex items-center py-2 px-8"
+                href="/testcases/create"
+              >
+                <Icon icon="file" width="16" height="16" className="mr-2" />
+                Chrome on Enterprise
+                <span className="ml-auto text-sm text-gray-500">
+                  0% completed
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="flex items-center py-2 px-8"
+                href="/testcases/123/view"
+              >
+                <Icon icon="file" width="16" height="16" className="mr-2" />
+                Firefox on SaaS
+                <span className="ml-auto text-sm text-gray-500">
+                  11% completed
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </details>
+      </Fragment>
+    );
+  }
 }
 
 export default Dashboard;
