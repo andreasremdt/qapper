d4c232637b7f872c931ff517950388ed45874b64
Andreas Remdt <andreas.remdt@gmail.com>
2019-11-28 17:09:39 +0100

    chore(bundler): optimize css, include tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,25 @@
+var purgecss = require("@fullhuman/postcss-purgecss");
+var tailwind = require("tailwindcss");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
-  plugins: [require("tailwindcss")]
+  plugins: [
+    tailwind(),
+    process.env.NODE_ENV == "production"
+      ? purgecss({
+          content: ["./index.html", "./js/**/*.js"],
+          extractors: [
+            {
+              extractor: TailwindExtractor,
+              extensions: ["html", "js"]
+            }
+          ]
+        })
+      : () => []
+  ]
 };
